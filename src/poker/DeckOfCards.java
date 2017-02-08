79974423d222b0b0c4e9c90e4fd0e9a48af7c126
Gavin Keaveney <gavin.keaveney@ucdconnect.ie>
2017-02-08 15:52:57 +0000

    Final Submission for Assignment 2

--- src/poker/DeckOfCards.java
+++ src/poker/DeckOfCards.java
@@ -1,7 +1,82 @@
 package poker;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  * Created by Gavin on 01/02/2017.
  */
 public class DeckOfCards {
+    private ArrayList<PlayingCard> deck = new ArrayList<>();
+    private ArrayList<PlayingCard> returned = new ArrayList<>();
+    private int deal = -1;
+    private int returnedTracker = 0;
+
+
+    DeckOfCards () {
+
+    }
+
+    void reset() {
+        String[] cardTypes = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+
+        int currentValue = 14; // set gameValue for Ace = 14
+
+        for (int i = 0; i< cardTypes.length; i++){
+            deck.add(new PlayingCard(cardTypes[i], PlayingCard.HEARTS, i+1, currentValue));
+            currentValue = i+2;
+        }
+
+        currentValue = 14;
+        for (int i = 0; i< cardTypes.length; i++){
+            deck.add(new PlayingCard(cardTypes[i], PlayingCard.DIAMONDS, i+1, currentValue));
+            currentValue = i+2;
+        }
+        currentValue = 14;
+        for (int i = 0; i< cardTypes.length; i++) {
+            deck.add(new PlayingCard(cardTypes[i], PlayingCard.CLUBS, i + 1, currentValue));
+            currentValue = i + 2;
+        }
+        currentValue = 14;
+        for (int i = 0; i< cardTypes.length; i++) {
+            deck.add(new PlayingCard(cardTypes[i], PlayingCard.SPADES, i + 1, currentValue));
+            currentValue = i + 2;
+        }
+
+        for (int i = 0; i<2705; i++) { // call shuffle 2704 times
+            shuffle();
+        }
+
+    }
+
+    void shuffle() {
+
+        ArrayList<PlayingCard> temp = new ArrayList<>();
+//        Random random1 = new Random();
+
+        while(!deck.isEmpty()) {
+            int pos1=(int)(Math.random()*deck.size());
+            temp.add(deck.get(pos1));
+            deck.remove(pos1);
+        }
+        deck=temp;
+    }
+
+
+
+    PlayingCard dealNext() {
+        deal++;
+        PlayingCard toDeal = new PlayingCard();
+
+        toDeal = deck.get(deal);
+//        deck.remove(deal);
+
+        return toDeal;
+    }
+
+    void returnCard(PlayingCard discarded) {
+
+        returned.add(returnedTracker++, discarded);
+    }
+
 }
--- src/poker/Main.java
+++ src/poker/Main.java
@@ -6,37 +6,20 @@ public class Main {
 
     public static void main(String[] args) {
 
-//        String[] noOfSuits = {HEARTS, DIAMONDS, CLUBS, SPADES};
-        String[] cardTypes = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
-        PlayingCard card = new PlayingCard();
-        ArrayList<PlayingCard> deck = new ArrayList<>();
-        int currentValue = 14;
+        DeckOfCards dealDeck = new DeckOfCards();
+        ArrayList<PlayingCard> hand = new ArrayList<>();
 
-        for (int i = 0; i< cardTypes.length; i++){
-            deck.add(new PlayingCard(cardTypes[i], PlayingCard.HEARTS, i+1, currentValue));
-            currentValue = i+2;
-        }
-
-        currentValue = 14;
-        for (int i = 0; i< cardTypes.length; i++){
-            deck.add(new PlayingCard(cardTypes[i], PlayingCard.DIAMONDS, i+1, currentValue));
-            currentValue = i+2;
-        }
-        currentValue = 14;
-        for (int i = 0; i< cardTypes.length; i++) {
-            deck.add(new PlayingCard(cardTypes[i], PlayingCard.CLUBS, i + 1, currentValue));
-            currentValue = i + 2;
-        }
-            currentValue = 14;
-        for (int i = 0; i< cardTypes.length; i++) {
-            deck.add(new PlayingCard(cardTypes[i], PlayingCard.SPADES, i + 1, currentValue));
-            currentValue = i + 2;
+        dealDeck.reset();
+        for (int i=0; i<52; i++) {
+            if (i % 2 == 0) {
+                hand.add(dealDeck.dealNext());
+            }
+            else {
+                dealDeck.returnCard(dealDeck.dealNext()); // return every second card
+            }
         }
 
-        for (int i=0; i<deck.size(); i++){
-            card = deck.get(i);
-            System.out.println(card.toString());
+        System.out.println("Hand: " + hand);
 
-        }
     }
 }
