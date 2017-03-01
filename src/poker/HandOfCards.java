94f6f48413e4e0b3a2849ba8bf222523e014cb6c
Gavin Keaveney <gavin.keaveney@ucdconnect.ie>
2017-03-01 13:01:09 +0000

    Adding basic card return functionality Adds basic framework for deciding on returned cards

--- src/poker/HandOfCards.java
+++ src/poker/HandOfCards.java
@@ -3,7 +3,6 @@ package poker;
 import java.util.ArrayList;
 import java.util.Collections;
 
-
 public class HandOfCards {
     public static int HAND_SIZE = 5;
     private static int ACE_GAME_VALUE = 14;
@@ -36,6 +35,8 @@ public class HandOfCards {
     private boolean isOnePair = false;
     private boolean isHighHand = false;
 
+    private int sumGameValue = 0;
+    private int highCard = 0;
 
     HandOfCards(DeckOfCards deck) {
 
@@ -54,162 +55,6 @@ public class HandOfCards {
                 }
             }
         }
-    }
-
-    // HAND RANKING BELOW
-
-    public int getGameValue(){
-
-        int sumGameValue = 0;
-        int highCard = 0;
-        int i = 0;
-
-        if (isRoyalFlush) {
-
-            return ROYAL_FLUSH_WEIGHT; // Highest hand, if draw split pot
-
-        } else if (isStraightFlush) {
-
-            highCard = hand.get(0).getGameValue();
-            return STRAIGHT_FLUSH_WEIGHT + highCard; // Winner will depend on high card
-
-        } else if (isFourOfAKind) {
-
-            if (hand.get(0).getGameValue() == hand.get(1).getGameValue()) {
-                highCard = hand.get(0).getGameValue();
-            } else {
-                highCard = hand.get(1).getGameValue();
-            }
-            return FOUR_OF_A_KIND_WEIGHT + highCard; // Checks position of high card value, winner has highest match
-
-        } else if (isFullHouse) {
-
-            if ((hand.get(2).getGameValue() == (hand.get(3).getGameValue())) && ((hand.get(3).getGameValue()) == (hand.get(4).getGameValue()))) { // 3=4=5
-                highCard = hand.get(2).getGameValue();
-            } else { // 1=2=3
-                highCard = hand.get(0).getGameValue();
-            }
-
-            return FULL_HOUSE_WEIGHT + highCard; // Highest 3 pair wins
-
-        } else if (isFlush) {
-
-            for (i=0;i<HAND_SIZE;i++) {
-                sumGameValue += hand.get(i).getGameValue();
-            }
-            return FLUSH_WEIGHT + sumGameValue; // Highest ranked hand wins
-
-        }else if (isStraight) {
-
-            highCard = hand.get(0).getGameValue();
-
-            return STRAIGHT_WEIGHT + highCard; // Winner depends on high card, draw split pot
-
-        }else if (isThreeOfAKind) {
-
-            if (hand.get(0).getGameValue() == (hand.get(2).getGameValue())) { // 0&2
-                highCard = hand.get(0).getGameValue();
-            } else { // 2&4
-                highCard = (hand.get(2).getGameValue());
-            }
-
-            return THREE_OF_A_KIND_WEIGHT + highCard; // Highest matching three wins
-
-        }else if (isTwoPair) {
-
-            // Find highest pair and assign hand value based on it
-            // Clearance required so higher card pair wins
-            // Double clearance to ensure higher card pair wins
-            // Adds spare card for draw situations
-
-            if (hand.get(0).getGameValue() == hand.get(1).getGameValue() && hand.get(2).getGameValue() == hand.get(3).getGameValue()) { // 0&1,2&3
-                if (hand.get(0).getGameValue() > hand.get(2).getGameValue()) {
-                    sumGameValue = (hand.get(0).getGameValue() + (HIGH_CARD_CLEARANCE*2) * (hand.get(0).getGameValue() + (HIGH_CARD_CLEARANCE*2)));
-                    sumGameValue += (hand.get(2).getGameValue() + (HIGH_CARD_CLEARANCE) * (hand.get(2).getGameValue() + (HIGH_CARD_CLEARANCE)));
-                    sumGameValue += hand.get(4).getGameValue();
-
-                } else {
-                    sumGameValue = (hand.get(2).getGameValue() + (HIGH_CARD_CLEARANCE*2) * (hand.get(2).getGameValue() + (HIGH_CARD_CLEARANCE*2)));
-                    sumGameValue += (hand.get(0).getGameValue() + (HIGH_CARD_CLEARANCE) * (hand.get(0).getGameValue() + (HIGH_CARD_CLEARANCE)));
-                    sumGameValue += hand.get(4).getGameValue();
-
-                }
-            } else if (hand.get(0).getGameValue() == hand.get(1).getGameValue() && hand.get(3).getGameValue() == hand.get(4).getGameValue()) { // 0&1,3&4
-                if (hand.get(0).getGameValue() > hand.get(3).getGameValue()) {
-                    sumGameValue = (hand.get(0).getGameValue() + (HIGH_CARD_CLEARANCE*2) * (hand.get(0).getGameValue() + (HIGH_CARD_CLEARANCE*2)));
-                    sumGameValue += (hand.get(3).getGameValue() + (HIGH_CARD_CLEARANCE) * (hand.get(3).getGameValue() + (HIGH_CARD_CLEARANCE)));
-                    sumGameValue += hand.get(2).getGameValue();
-
-                } else {
-                    sumGameValue = (hand.get(3).getGameValue() + (HIGH_CARD_CLEARANCE*2) * (hand.get(3).getGameValue() + (HIGH_CARD_CLEARANCE*2)));
-                    sumGameValue += (hand.get(0).getGameValue() + (HIGH_CARD_CLEARANCE) * (hand.get(0).getGameValue() + (HIGH_CARD_CLEARANCE)));
-                    sumGameValue += hand.get(2).getGameValue();
-
-                }
-            } else if (hand.get(1).getGameValue() == hand.get(2).getGameValue() && hand.get(3).getGameValue() == hand.get(4).getGameValue()) {// 1&2,3&4
-                if (hand.get(1).getGameValue() > hand.get(3).getGameValue()) {
-                    sumGameValue = (hand.get(1).getGameValue() + (HIGH_CARD_CLEARANCE*2) * (hand.get(1).getGameValue() + (HIGH_CARD_CLEARANCE*2)));
-                    sumGameValue += (hand.get(3).getGameValue() + (HIGH_CARD_CLEARANCE) * (hand.get(3).getGameValue() + (HIGH_CARD_CLEARANCE)));
-                    sumGameValue += hand.get(0).getGameValue();
-
-                } else {
-                    sumGameValue = (hand.get(3).getGameValue() + (HIGH_CARD_CLEARANCE*2) * (hand.get(3).getGameValue() + (HIGH_CARD_CLEARANCE*2)));
-                    sumGameValue += (hand.get(1).getGameValue() + (HIGH_CARD_CLEARANCE) * (hand.get(1).getGameValue() + (HIGH_CARD_CLEARANCE)));
-                    sumGameValue += hand.get(0).getGameValue();
-
-                }
-            }
-
-            return TWO_PAIRS_WEIGHT + sumGameValue; // Highest pair - highest second pair - highest side card
-
-        }else if (isOnePair) {
-
-            // Gets value of pair
-            // Multiply with Clearance to ensure high card pair wins
-            // Adds rest of cards for draw situations - could be refined
-
-            for (i=0;i<HAND_SIZE-1;i++){
-                if (hand.get(i).getGameValue() == hand.get(i+1).getGameValue()) {
-                    highCard = hand.get(i).getGameValue();
-                }
-            }
-
-            highCard = (highCard+HIGH_CARD_CLEARANCE) * (highCard+HIGH_CARD_CLEARANCE);
-
-            for (i=0;i<HAND_SIZE;i++) {
-                highCard += hand.get(i).getGameValue();
-            }
-
-
-            return ONE_PAIR_WEIGHT + highCard; // highest pair - highest side - next highest - next highest
-
-        }else if (isHighHand) {
-
-            // Requires some refinement
-            // Weights higher cards to ensure highest card wins
-
-            sumGameValue = hand.get(0).getGameValue() * hand.get(0).getGameValue() * HIGH_CARD_CLEARANCE * 250;
-            sumGameValue += hand.get(1).getGameValue() * hand.get(1).getGameValue() * HIGH_CARD_CLEARANCE * 100;
-            sumGameValue += hand.get(2).getGameValue() * hand.get(2).getGameValue() * HIGH_CARD_CLEARANCE * 10;
-            sumGameValue += hand.get(3).getGameValue() * hand.get(3).getGameValue() * HIGH_CARD_CLEARANCE;
-            sumGameValue +=  hand.get(4).getGameValue();
-
-            return HIGH_HAND_WEIGHT + sumGameValue;
-
-        }else {
-            return -1;
-        }
-
-        //return handGameValue;
-    }
-
-    public String toString() {
-
-        String handString = hand.get(0).toString() + " ";
-        for (int i = 1; i<HAND_SIZE; i++) {
-            handString += hand.get(i).toString() + " ";
-        }
-        return handString;
     }
 
     public String checkHand() {
@@ -247,7 +92,7 @@ public class HandOfCards {
         }
     }
 
-// Public method to set hand to test checkHand
+    // Public method to set hand to test checkHand
     public void setHand(int index, PlayingCard card) {
         hand.add(index, card);
     }
@@ -271,16 +116,21 @@ public class HandOfCards {
         }
     }
     private boolean isFourOfAKind() {
-        if ((hand.get(0).getGameValue() == hand.get(3).getGameValue()) || (hand.get(1).getGameValue() == hand.get(4).getGameValue())) {
+        if ((hand.get(0).getGameValue() == hand.get(3).getGameValue()) ||
+                (hand.get(1).getGameValue() == hand.get(4).getGameValue())) {
             return true; // 0&3,1&4
         } else {
             return false;
         }
     }
     private boolean isFullHouse() {
-        if ((hand.get(0).getGameValue() == (hand.get(1).getGameValue())) && ((hand.get(2).getGameValue() == (hand.get(3).getGameValue())) && ((hand.get(3).getGameValue()) == (hand.get(4).getGameValue())))) {
+        if ((hand.get(0).getGameValue() == (hand.get(1).getGameValue())) &&
+                ((hand.get(2).getGameValue() == (hand.get(3).getGameValue())) &&
+                        ((hand.get(3).getGameValue()) == (hand.get(4).getGameValue())))) {
             return true; // 1=2, 3=4=5
-        } else if ((((hand.get(0).getGameValue()) == (hand.get(1).getGameValue())) && (hand.get(1).getGameValue() == (hand.get(2).getGameValue()))) && (hand.get(3).getGameValue() == (hand.get(4).getGameValue()))) {
+        } else if ((((hand.get(0).getGameValue()) == (hand.get(1).getGameValue())) &&
+                (hand.get(1).getGameValue() == (hand.get(2).getGameValue()))) &&
+                (hand.get(3).getGameValue() == (hand.get(4).getGameValue()))) {
             return true; // 1=2=3,4=5
         } else {
             return false;
@@ -301,7 +151,9 @@ public class HandOfCards {
         return false;
     }
     private boolean isStraight() {
-        if ((hand.get(0).getGameValue() - hand.get(1).getGameValue() == 1) && (hand.get(1).getGameValue() - hand.get(2).getGameValue() == 1) && (hand.get(2).getGameValue() - hand.get(3).getGameValue() == 1) && (hand.get(3).getGameValue() - hand.get(4).getGameValue() == 1)) {
+        if ((hand.get(0).getGameValue() - hand.get(1).getGameValue() == 1) && (hand.get(1).getGameValue() -
+                hand.get(2).getGameValue() == 1) && (hand.get(2).getGameValue() - hand.get(3).getGameValue() == 1) &&
+                (hand.get(3).getGameValue() - hand.get(4).getGameValue() == 1)) {
             return true;
         } else if (hand.get(0).getGameValue() - hand.get(1).getGameValue() == 1) {
             if (hand.get(1).getGameValue() - hand.get(2).getGameValue() == 1) {
@@ -347,8 +199,229 @@ public class HandOfCards {
             return false;
         }
     }
-    private boolean isHighHand() { // Last option, will always be true if checked
-        return true;
+
+    // HAND RANKING BELOW
+    // Each ranking keeps score under superior hands Weighting value indicated above
+    // Eack ranking rates each hand on rules to ensure correct hand wins e.g. Highest Card in Straight Flush
+
+    public int getGameValue(){
+
+        int i = 0;
+
+        if (isRoyalFlush) {
+
+            return ROYAL_FLUSH_WEIGHT; // Highest hand, if draw split pot
+
+        } else if (isStraightFlush) {
+
+            highCard = hand.get(0).getGameValue();
+            return STRAIGHT_FLUSH_WEIGHT + highCard; // Winner will depend on high card
+
+        } else if (isFourOfAKind) {
+
+            if (hand.get(0).getGameValue() == hand.get(1).getGameValue()) {
+                highCard = hand.get(0).getGameValue();
+            } else {
+                highCard = hand.get(1).getGameValue();
+            }
+            return FOUR_OF_A_KIND_WEIGHT + highCard; // Checks position of high card value, winner has highest match
+
+        } else if (isFullHouse) {
+
+            if ((hand.get(2).getGameValue() == (hand.get(3).getGameValue())) &&
+                    ((hand.get(3).getGameValue()) == (hand.get(4).getGameValue()))) { // 3=4=5
+                highCard = hand.get(2).getGameValue();
+            } else { // 1=2=3
+                highCard = hand.get(0).getGameValue();
+            }
+
+            return FULL_HOUSE_WEIGHT + highCard; // Highest 3 pair wins
+
+        } else if (isFlush) {
+
+            for (i=0;i<HAND_SIZE;i++) {
+                sumGameValue += hand.get(i).getGameValue();
+            }
+            return FLUSH_WEIGHT + sumGameValue; // Highest ranked hand wins
+
+        }else if (isStraight) {
+
+            highCard = hand.get(0).getGameValue();
+
+            return STRAIGHT_WEIGHT + highCard; // Winner depends on high card, draw split pot
+
+        }else if (isThreeOfAKind) {
+
+            if (hand.get(0).getGameValue() == (hand.get(2).getGameValue())) { // 0&2
+                highCard = hand.get(0).getGameValue();
+            } else { // 2&4
+                highCard = (hand.get(2).getGameValue());
+            }
+
+            return THREE_OF_A_KIND_WEIGHT + highCard; // Highest matching three wins
+
+        }else if (isTwoPair) {
+
+            return twoPairValue();
+
+        }else if (isOnePair) {
+
+            return onePairValue();
+
+        }else if (isHighHand) {
+
+           return highHandValue();
+
+        }else {
+            return -1;
+        }
+
+        //return handGameValue;
+    }
+
+    /*
+
+    private boolean isRoyalFlush = false; done
+    private boolean isStraightFlush = false; done
+    private boolean isFourOfAKind = false; done
+    private boolean isFullHouse = false; done
+    private boolean isFlush = false; done
+    private boolean isStraight = false; done
+    private boolean isThreeOfAKind = false;
+    private boolean isTwoPair = false;
+    private boolean isOnePair = false;
+    private boolean isHighHand = false; done
+
+     */
+
+    public int getDiscardProbability(int cardPosition) {
+        // Return value between 0 and 100
+
+        if (cardPosition >= 0 && cardPosition < 5) {
+
+            if (isRoyalFlush || isStraightFlush || isFourOfAKind || isFullHouse || isFlush || isStraight) {
+                // no improvement likely/possible
+
+                return 0;
+            } else if (isThreeOfAKind || isTwoPair || isOnePair) {
+                // return lower of non matching
+                // keep pairs
+                // return value based on card ranking
+
+                if (cardPosition == 0) {
+                    if (hand.get(cardPosition).getGameValue() == hand.get(cardPosition+1).getGameValue()) {
+                        return 0;
+                    } else {
+                        return 100 - (hand.get(cardPosition).getGameValue() * 5);
+                    }
+                } else if (cardPosition == 4) {
+                    if (hand.get(cardPosition).getGameValue() == hand.get(cardPosition-1).getGameValue()) {
+                        return 0;
+                    } else {
+                        return 100 - (hand.get(cardPosition).getGameValue() * 5);
+                    }
+                } else if (hand.get(cardPosition).getGameValue() == hand.get(cardPosition-1).getGameValue() ||
+                        hand.get(cardPosition).getGameValue() == hand.get(cardPosition+1).getGameValue()) { //cardPosition 1,2,3
+
+                    return 0;
+                } else {
+
+                    return 100 - (hand.get(cardPosition).getGameValue() * 5);
+                }
+            } else {
+                // high hand
+                // return based on card rank
+
+                return 100 - (hand.get(cardPosition).getGameValue() * 5);
+
+            }
+        } else {
+
+            return 0;
+        }
+    }
+
+    private int highHandValue() {
+        // Requires some refinement
+        // Weights higher cards to ensure highest card wins
+        // Keeps score under 1.4 million
+        sumGameValue = 0;
+
+        sumGameValue = hand.get(0).getGameValue() * hand.get(0).getGameValue() * HIGH_CARD_CLEARANCE * 250;
+        sumGameValue += hand.get(1).getGameValue() * hand.get(1).getGameValue() * HIGH_CARD_CLEARANCE * 100;
+        sumGameValue += hand.get(2).getGameValue() * hand.get(2).getGameValue() * HIGH_CARD_CLEARANCE * 10;
+        sumGameValue += hand.get(3).getGameValue() * hand.get(3).getGameValue() * HIGH_CARD_CLEARANCE;
+        sumGameValue +=  hand.get(4).getGameValue();
+
+
+        return HIGH_HAND_WEIGHT + sumGameValue;
+    }
+
+    private int onePairValue() {
+        // Gets value of pair
+        // Multiply with Clearance to ensure high card pair wins
+        // Adds rest of cards for draw situations - could be refined
+        int i = 0;
+        highCard = 0;
+        sumGameValue = 0;
+
+
+        for (i=0;i<HAND_SIZE-1;i++){
+            if (hand.get(i).getGameValue() == hand.get(i+1).getGameValue()) {
+                highCard = hand.get(i).getGameValue();
+            }
+        }
+
+        sumGameValue = (highCard + (HIGH_CARD_CLEARANCE * HIGH_CARD_CLEARANCE));
+
+        for (i=0;i<HAND_SIZE;i++) {
+            sumGameValue += hand.get(i).getGameValue();
+        }
+
+
+        return ONE_PAIR_WEIGHT + sumGameValue; // highest pair - highest side - next highest - next highest
+    }
+
+    private int twoPairValue() {
+        // Find highest pair and assign hand value based on it
+        // Clearance required so higher card pair wins
+        // Clearance squared to ensure higher card pair wins
+        // Adds spare card for draw situations
+        sumGameValue = 0;
+
+        if (hand.get(0).getGameValue() == hand.get(1).getGameValue() && hand.get(2).getGameValue() == hand.get(3).getGameValue()) { // 0&1,2&3
+            if (hand.get(0).getGameValue() > hand.get(2).getGameValue()) {
+                sumGameValue = (hand.get(0).getGameValue() * HIGH_CARD_CLEARANCE * HIGH_CARD_CLEARANCE);
+                sumGameValue += (hand.get(2).getGameValue() * HIGH_CARD_CLEARANCE);
+                sumGameValue += hand.get(4).getGameValue();
+
+            }
+        } else if (hand.get(0).getGameValue() == hand.get(1).getGameValue() && hand.get(3).getGameValue() == hand.get(4).getGameValue()) { // 0&1,3&4
+            if (hand.get(0).getGameValue() > hand.get(3).getGameValue()) {
+                sumGameValue = (hand.get(0).getGameValue() * HIGH_CARD_CLEARANCE * HIGH_CARD_CLEARANCE);
+                sumGameValue += (hand.get(3).getGameValue() * HIGH_CARD_CLEARANCE);
+                sumGameValue += hand.get(2).getGameValue();
+
+            }
+        } else if (hand.get(1).getGameValue() == hand.get(2).getGameValue() && hand.get(3).getGameValue() == hand.get(4).getGameValue()) {// 1&2,3&4
+            if (hand.get(1).getGameValue() > hand.get(3).getGameValue()) {
+                sumGameValue = (hand.get(1).getGameValue() * HIGH_CARD_CLEARANCE * HIGH_CARD_CLEARANCE);
+                sumGameValue += (hand.get(3).getGameValue() * HIGH_CARD_CLEARANCE);
+                sumGameValue += hand.get(0).getGameValue();
+
+            }
+        }
+
+        return TWO_PAIRS_WEIGHT + sumGameValue; // Highest pair - highest second pair - highest side card
+    }
+
+    public String toString() {
+
+        String handString = hand.get(0).toString() + " ";
+        for (int i = 1; i<HAND_SIZE; i++) {
+            handString += hand.get(i).toString() + " ";
+        }
+        return handString;
     }
 
 }
--- src/poker/Main.java
+++ src/poker/Main.java
@@ -14,23 +14,47 @@ public class Main {
         HandOfCards hand = new HandOfCards(dealDeck);
 
 //  Hand setter and test output
+// Added second hand to test close hands for correct ranking
         HandOfCards testHand = new HandOfCards(dealDeck);
+        HandOfCards testHand2 = new HandOfCards(dealDeck);
 
+
+        // testHand1
         testHand.setHand(0, new PlayingCard("A", PlayingCard.CLUBS, 1, 14));
         testHand.setHand(1, new PlayingCard("K", PlayingCard.HEARTS, 13, 13));
         testHand.setHand(2, new PlayingCard("Q", PlayingCard.HEARTS, 12, 12));
-        testHand.setHand(3, new PlayingCard("J", PlayingCard.HEARTS, 11, 11));
+        testHand.setHand(3, new PlayingCard("J", PlayingCard.HEARTS, 11, 10));
         testHand.setHand(4, new PlayingCard("9", PlayingCard.HEARTS, 9, 9));
 
         System.out.println("Hand: " + testHand.toString());
         System.out.println(testHand.checkHand());
-        System.out.println("Hand value: " + testHand.getGameValue());
+        System.out.println("Hand value: " + testHand.getGameValue() + "\n");
+        for (int i=0; i<5; i++){
+            System.out.println(testHand.getDiscardProbability(i));
+        }
+
+        // testHand2
+        testHand2.setHand(0, new PlayingCard("A", PlayingCard.CLUBS, 1, 14));
+        testHand2.setHand(1, new PlayingCard("K", PlayingCard.HEARTS, 13, 13));
+        testHand2.setHand(2, new PlayingCard("Q", PlayingCard.HEARTS, 12, 12));
+        testHand2.setHand(3, new PlayingCard("J", PlayingCard.HEARTS, 11, 11));
+        testHand2.setHand(4, new PlayingCard("8", PlayingCard.HEARTS, 8, 8));
+
+        System.out.println("Hand: " + testHand2.toString());
+        System.out.println(testHand2.checkHand());
+        System.out.println("Hand value: " + testHand2.getGameValue() + "\n");
+        for (int i=0; i<5; i++){
+            System.out.println(testHand2.getDiscardProbability(i));
+        }
 
 
 
         System.out.println("Hand: " + hand.toString());
         System.out.println(hand.checkHand());
         System.out.println("Hand value: " + hand.getGameValue());
+        for (int i=0; i<5; i++){
+            System.out.println(hand.getDiscardProbability(i));
+        }
 
     }
 }
